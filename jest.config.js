eb6cd658dca456b3077680f8f01d6539049f90b4
avesheva <shevochkandrey@gmail.com>
2022-12-29 21:35:53 +0200

    Install jest and basic set up for tests.

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+}
---
+++ tests/YorLibNameTest.spec.ts
@@ -0,0 +1,1 @@
+test('Your lib first test ', () => {})
