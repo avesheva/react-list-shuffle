e2a508727c7f2dec0e308da30a7abb5edf87d666
avesheva <shevochkandrey@gmail.com>
2023-01-19 17:50:54 +0200

    Implemented basic functionality.

--- lib/components/ListShuffle.tsx
+++ lib/components/ListShuffle.tsx
@@ -1,9 +1,79 @@
-import React, { FC } from 'react'
+import React, { FC, ReactNode, useEffect, useRef } from 'react'
 
-const ListShuffle: FC = () => {
+export interface IProps {
+  id?: string,
+  duration?: number,
+  shuffleOnInit?: boolean,
+  shuffle: number | string | boolean,
+  children: ReactNode
+}
+
+export type ListItemDataType = { index: number, top: number }
+
+let mounted = false
+
+const ListShuffle: FC<IProps> = ({
+  children,
+  id = 'listWrapper',
+  duration = 1,
+  shuffleOnInit = false,
+  shuffle,
+}) => {
+  const listWrapper = useRef<HTMLDivElement | null>(null)
+  const arr = useRef<ListItemDataType[]>([])
+
+  const shuffleArray = (array: ListItemDataType[]) => {
+    const arrClone = [...array]
+
+    for (let i = arrClone.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      const t = arrClone[i]; arrClone[i] = arrClone[j]; arrClone[j] = t
+    }
+
+    return arrClone
+  }
+
+  const shuffleList = () => {
+    if (!listWrapper.current) return
+
+    const newOrder = shuffleArray(arr.current)
+
+    for (let i = 0; i < listWrapper.current.children.length; i++) {
+      const currentElement = listWrapper.current.children[i] as HTMLElement
+      const newIndex = newOrder.findIndex((item  ) => item.index === i)
+      const top = arr.current[newIndex].top - arr.current[i].top
+
+      currentElement.style.transform = `translate(0, ${ top }px)`
+    }
+  }
+
+  useEffect(() => {
+    listWrapper.current = document.getElementById(id) as HTMLDivElement
+
+    if (!mounted && listWrapper) {
+      for (let i = 0; i < listWrapper.current.children.length; i++) {
+        const element = listWrapper.current.children[i] as HTMLElement
+
+        element.setAttribute('ps', `${ i }`)
+        element.style.transitionProperty = 'transform'
+        element.style.transitionDuration = `${ duration }s`
+
+        arr.current.push({ index: i, top: element.getBoundingClientRect().top })
+      }
+
+      mounted = true
+    }
+  }, [])
+
+  useEffect(() => {
+    if (shuffleOnInit || shuffle !== 1) {
+      shuffleList()
+    }
+  }, [shuffle])
+
   return (
-    <div>
-      <h4>List Shuffle</h4>
+    <div id={ id }>
+      { children }
     </div>
   )
 }
