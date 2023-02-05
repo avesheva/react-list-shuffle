f0e4f7185e654ba1cfc466ed6c03caa164f9d24a
avesheva <shevochkandrey@gmail.com>
2023-02-05 15:53:34 +0200

    Changes to fix publish issues

--- vite.config.ts
+++ vite.config.ts
@@ -10,14 +10,7 @@ export default defineConfig({
       fileName: format => `react-list-shuffle.${ format }.js`,
     },
     rollupOptions: {
-      external: ['React'],
-      output: {
-        globals: {
-          react: 'React',
-        },
-      },
+      external: ['react'],
     },
   },
-
-  plugins: [react()],
 })
